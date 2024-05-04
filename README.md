610adca25ac60fdf09da4b1722a917e4fd94afbf
willwang <jiayewang65@gmail.com>
2024-05-03 21:04:45 -0600

    Custom Typeracer plot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# typeracer-result-plotter
+# Typeracer Result Plotter
+
+Typeracer recently updated the "Improvement Over Time" plot in the "My Scores" tab, but I don't think it accurately captures my typing history compared to the older plots I have saved. So, I've decided to create my own version.
+
+The original plot was a line graph displaying changes in words per minute across all my races. In my custom version, I've shifted the focus to track performance over time, rather than just across races. Additionally, I've added a line to represent accuracy, which I believe is equally important.
+
+![](race_plot_May032024.png)
---
+++ plotter.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+df = pd.read_csv('race_data.csv', parse_dates=['Date/Time (UTC)'])
+
+df.sort_values('Date/Time (UTC)', inplace=True)
+
+window_size = 500
+df['WPM_Smooth'] = df['WPM'].rolling(window=window_size).mean()
+df['Accuracy_Smooth'] = df['Accuracy'].rolling(window=window_size).mean()
+formatted_date = datetime.now().strftime("%b. %d, %Y")
+
+plt.figure(figsize=(14, 7))
+ax1 = plt.gca()  # WPM
+ax2 = ax1.twinx()  # Accuracy
+
+ax1.plot(df['Date/Time (UTC)'], df['WPM_Smooth'], linestyle='-', color='b', label='WPM (Smoothed)')
+ax1.set_xlabel('Date/Time (UTC)')
+ax1.set_ylabel('Words Per Minute (WPM)', color='b')
+ax1.tick_params(axis='y', labelcolor='b')
+ax1.grid(True)
+
+ax2.plot(df['Date/Time (UTC)'], df['Accuracy_Smooth'], linestyle='-', color='r', label='Accuracy (Smoothed)')
+ax2.set_ylabel('Accuracy', color='r')
+ax2.tick_params(axis='y', labelcolor='r')
+
+plt.title(f'Words Per Minute and Accuracy Over Time by {formatted_date}')
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
